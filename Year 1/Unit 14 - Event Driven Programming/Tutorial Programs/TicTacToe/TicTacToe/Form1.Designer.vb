7db839f6f1fc2041a4b5b2b471c45898137359af
Abidon Jude Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-04-01 18:28:42 +0100

    Add files via upload

---
+++ Year 1/Unit 14 - Event Driven Programming/Tutorial Programs/TicTacToe/TicTacToe/Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnTic1 = New System.Windows.Forms.Button()
+        Me.btnTic2 = New System.Windows.Forms.Button()
+        Me.btnTic3 = New System.Windows.Forms.Button()
+        Me.btnTic4 = New System.Windows.Forms.Button()
+        Me.btnTic5 = New System.Windows.Forms.Button()
+        Me.btnTic6 = New System.Windows.Forms.Button()
+        Me.btnTic7 = New System.Windows.Forms.Button()
+        Me.btnTic8 = New System.Windows.Forms.Button()
+        Me.btnTic9 = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.lblPlayerx = New System.Windows.Forms.Label()
+        Me.lblPlayer0 = New System.Windows.Forms.Label()
+        Me.lblCountX = New System.Windows.Forms.Label()
+        Me.lblCount0 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnTic1
+        '
+        Me.btnTic1.Location = New System.Drawing.Point(16, 32)
+        Me.btnTic1.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnTic1.Name = "btnTic1"
+        Me.btnTic1.Size = New System.Drawing.Size(100, 100)
+        Me.btnTic1.TabIndex = 0
+        Me.btnTic1.UseVisualStyleBackColor = True
+        '
+        'btnTic2
+        '
+        Me.btnTic2.Location = New System.Drawing.Point(124, 32)
+        Me.btnTic2.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnTic2.Name = "btnTic2"
+        Me.btnTic2.Size = New System.Drawing.Size(100, 100)
+        Me.btnTic2.TabIndex = 0
+        Me.btnTic2.UseVisualStyleBackColor = True
+        '
+        'btnTic3
+        '
+        Me.btnTic3.Location = New System.Drawing.Point(232, 32)
+        Me.btnTic3.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnTic3.Name = "btnTic3"
+        Me.btnTic3.Size = New System.Drawing.Size(100, 100)
+        Me.btnTic3.TabIndex = 0
+        Me.btnTic3.UseVisualStyleBackColor = True
+        '
+        'btnTic4
+        '
+        Me.btnTic4.Location = New System.Drawing.Point(16, 140)
+        Me.btnTic4.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnTic4.Name = "btnTic4"
+        Me.btnTic4.Size = New System.Drawing.Size(100, 100)
+        Me.btnTic4.TabIndex = 0
+        Me.btnTic4.UseVisualStyleBackColor = True
+        '
+        'btnTic5
+        '
+        Me.btnTic5.Location = New System.Drawing.Point(124, 140)
+        Me.btnTic5.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnTic5.Name = "btnTic5"
+        Me.btnTic5.Size = New System.Drawing.Size(100, 100)
+        Me.btnTic5.TabIndex = 0
+        Me.btnTic5.UseVisualStyleBackColor = True
+        '
+        'btnTic6
+        '
+        Me.btnTic6.Location = New System.Drawing.Point(232, 140)
+        Me.btnTic6.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnTic6.Name = "btnTic6"
+        Me.btnTic6.Size = New System.Drawing.Size(100, 100)
+        Me.btnTic6.TabIndex = 0
+        Me.btnTic6.UseVisualStyleBackColor = True
+        '
+        'btnTic7
+        '
+        Me.btnTic7.Location = New System.Drawing.Point(16, 248)
+        Me.btnTic7.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnTic7.Name = "btnTic7"
+        Me.btnTic7.Size = New System.Drawing.Size(100, 100)
+        Me.btnTic7.TabIndex = 0
+        Me.btnTic7.UseVisualStyleBackColor = True
+        '
+        'btnTic8
+        '
+        Me.btnTic8.Location = New System.Drawing.Point(124, 248)
+        Me.btnTic8.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnTic8.Name = "btnTic8"
+        Me.btnTic8.Size = New System.Drawing.Size(100, 100)
+        Me.btnTic8.TabIndex = 0
+        Me.btnTic8.UseVisualStyleBackColor = True
+        '
+        'btnTic9
+        '
+        Me.btnTic9.Location = New System.Drawing.Point(232, 248)
+        Me.btnTic9.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnTic9.Name = "btnTic9"
+        Me.btnTic9.Size = New System.Drawing.Size(100, 100)
+        Me.btnTic9.TabIndex = 0
+        Me.btnTic9.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(66, 393)
+        Me.btnReset.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(200, 100)
+        Me.btnReset.TabIndex = 0
+        Me.btnReset.Text = "Restart Game"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'lblPlayerx
+        '
+        Me.lblPlayerx.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPlayerx.Location = New System.Drawing.Point(489, 76)
+        Me.lblPlayerx.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblPlayerx.Name = "lblPlayerx"
+        Me.lblPlayerx.Size = New System.Drawing.Size(136, 39)
+        Me.lblPlayerx.TabIndex = 1
+        Me.lblPlayerx.Text = "Player X: "
+        '
+        'lblPlayer0
+        '
+        Me.lblPlayer0.AutoSize = True
+        Me.lblPlayer0.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPlayer0.Location = New System.Drawing.Point(489, 203)
+        Me.lblPlayer0.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblPlayer0.Name = "lblPlayer0"
+        Me.lblPlayer0.Size = New System.Drawing.Size(106, 29)
+        Me.lblPlayer0.TabIndex = 1
+        Me.lblPlayer0.Text = "Player 0:"
+        '
+        'lblCountX
+        '
+        Me.lblCountX.AutoSize = True
+        Me.lblCountX.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCountX.Location = New System.Drawing.Point(633, 76)
+        Me.lblCountX.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblCountX.Name = "lblCountX"
+        Me.lblCountX.Size = New System.Drawing.Size(26, 29)
+        Me.lblCountX.TabIndex = 1
+        Me.lblCountX.Text = "0"
+        '
+        'lblCount0
+        '
+        Me.lblCount0.AutoSize = True
+        Me.lblCount0.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCount0.Location = New System.Drawing.Point(633, 203)
+        Me.lblCount0.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblCount0.Name = "lblCount0"
+        Me.lblCount0.Size = New System.Drawing.Size(26, 29)
+        Me.lblCount0.TabIndex = 1
+        Me.lblCount0.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(782, 553)
+        Me.Controls.Add(Me.lblCount0)
+        Me.Controls.Add(Me.lblCountX)
+        Me.Controls.Add(Me.lblPlayer0)
+        Me.Controls.Add(Me.lblPlayerx)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnTic9)
+        Me.Controls.Add(Me.btnTic8)
+        Me.Controls.Add(Me.btnTic6)
+        Me.Controls.Add(Me.btnTic7)
+        Me.Controls.Add(Me.btnTic5)
+        Me.Controls.Add(Me.btnTic4)
+        Me.Controls.Add(Me.btnTic3)
+        Me.Controls.Add(Me.btnTic2)
+        Me.Controls.Add(Me.btnTic1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnTic1 As Button
+    Friend WithEvents btnTic2 As Button
+    Friend WithEvents btnTic3 As Button
+    Friend WithEvents btnTic4 As Button
+    Friend WithEvents btnTic5 As Button
+    Friend WithEvents btnTic6 As Button
+    Friend WithEvents btnTic7 As Button
+    Friend WithEvents btnTic8 As Button
+    Friend WithEvents btnTic9 As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents lblPlayerx As Label
+    Friend WithEvents lblPlayer0 As Label
+    Friend WithEvents lblCountX As Label
+    Friend WithEvents lblCount0 As Label
+End Class
---
+++ Year 1/Unit 14 - Event Driven Programming/Tutorial Programs/TicTacToe/TicTacToe/Form1.vb
@@ -0,0 +1,140 @@
+﻿Public Class Form1
+    Dim flag As Boolean = False
+    Private Sub Checkers()
+        If btnTic1.Text = "X" And btnTic2.Text = "X" And btnTic3.Text = "X" Then
+            MsgBox("Player X wins!", MsgBoxStyle.Information)
+            lblCountX.Text = lblCountX.Text + 1
+        End If
+        If btnTic4.Text = "X" And btnTic5.Text = "X" And btnTic6.Text = "X" Then
+            MsgBox("Player X wins!", MsgBoxStyle.Information)
+            lblCountX.Text = lblCountX.Text + 1
+        End If
+        If btnTic7.Text = "X" And btnTic8.Text = "X" And btnTic9.Text = "X" Then
+            MsgBox("Player X wins!", MsgBoxStyle.Information)
+            lblCountX.Text = lblCountX.Text + 1
+        End If
+        If btnTic1.Text = "0" And btnTic2.Text = "0" And btnTic3.Text = "0" Then
+            MsgBox("Player 0 wins!", MsgBoxStyle.Information)
+            lblCount0.Text = lblCountX.Text + 1
+        End If
+        If btnTic4.Text = "0" And btnTic5.Text = "0" And btnTic6.Text = "0" Then
+            MsgBox("Player 0 wins!", MsgBoxStyle.Information)
+            lblCount0.Text = lblCountX.Text + 1
+        End If
+        If btnTic7.Text = "0" And btnTic8.Text = "0" And btnTic9.Text = "0" Then
+            MsgBox("Player 0 wins!", MsgBoxStyle.Information)
+            lblCount0.Text = lblCountX.Text + 1
+        End If
+
+    End Sub
+
+    Private Sub btnTic1_Click(sender As Object, e As EventArgs) Handles btnTic1.Click
+        If flag = False Then
+            btnTic1.Text = "X"
+            flag = True
+        Else
+            btnTic1.Text = "0"
+            flag = False
+        End If
+        Checkers()
+    End Sub
+
+    Private Sub btnTic2_Click(sender As Object, e As EventArgs) Handles btnTic2.Click
+        If flag = False Then
+            btnTic2.Text = "X"
+            flag = True
+        Else
+            btnTic2.Text = "0"
+            flag = False
+        End If
+        Checkers()
+    End Sub
+
+    Private Sub btnTic3_Click(sender As Object, e As EventArgs) Handles btnTic3.Click
+        If flag = False Then
+            btnTic3.Text = "X"
+            flag = True
+        Else
+            btnTic3.Text = "0"
+            flag = False
+        End If
+        Checkers()
+    End Sub
+
+    Private Sub btnTic4_Click(sender As Object, e As EventArgs) Handles btnTic4.Click
+        If flag = False Then
+            btnTic4.Text = "X"
+            flag = True
+        Else
+            btnTic4.Text = "0"
+            flag = False
+        End If
+        Checkers()
+    End Sub
+    Private Sub btnTic5_Click(sender As Object, e As EventArgs) Handles btnTic5.Click
+        If flag = False Then
+            btnTic5.Text = "X"
+            flag = True
+        Else
+            btnTic5.Text = "0"
+            flag = False
+        End If
+        Checkers()
+    End Sub
+
+    Private Sub btnTic6_Click(sender As Object, e As EventArgs) Handles btnTic6.Click
+        If flag = False Then
+            btnTic6.Text = "X"
+            flag = True
+        Else
+            btnTic6.Text = "0"
+            flag = False
+        End If
+        Checkers()
+    End Sub
+
+    Private Sub btnTic7_Click(sender As Object, e As EventArgs) Handles btnTic7.Click
+        If flag = False Then
+            btnTic7.Text = "X"
+            flag = True
+        Else
+            btnTic7.Text = "0"
+            flag = False
+        End If
+        Checkers()
+    End Sub
+
+    Private Sub btnTic8_Click(sender As Object, e As EventArgs) Handles btnTic8.Click
+        If flag = False Then
+            btnTic8.Text = "X"
+            flag = True
+        Else
+            btnTic8.Text = "0"
+            flag = False
+        End If
+        Checkers()
+    End Sub
+
+    Private Sub btnTic9_Click(sender As Object, e As EventArgs) Handles btnTic9.Click
+        If flag = False Then
+            btnTic9.Text = "X"
+            flag = True
+        Else
+            btnTic9.Text = "0"
+            flag = False
+        End If
+        Checkers()
+    End Sub
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        btnTic1.Text = ""
+        btnTic2.Text = ""
+        btnTic3.Text = ""
+        btnTic4.Text = ""
+        btnTic5.Text = ""
+        btnTic6.Text = ""
+        btnTic7.Text = ""
+        btnTic8.Text = ""
+        btnTic9.Text = ""
+
+    End Sub
+End Class
