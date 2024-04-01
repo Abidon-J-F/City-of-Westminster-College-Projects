a9cf8c6ce2231dbc9f1c5af7a6ffa49264f22ebc
Abidon Jude Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-04-01 18:09:18 +0100

    Add files via upload

---
+++ Year 1/Unit 14 - Event Driven Programming/Tutorial Programs/Exit/Exit/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnexit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblSum = New System.Windows.Forms.Label()
+        Me.txtFood = New System.Windows.Forms.TextBox()
+        Me.txtDrinks = New System.Windows.Forms.TextBox()
+        Me.btnTotal = New System.Windows.Forms.Button()
+        Me.btnreset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnexit
+        '
+        Me.btnexit.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnexit.Location = New System.Drawing.Point(1288, 613)
+        Me.btnexit.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(552, 201)
+        Me.btnexit.TabIndex = 0
+        Me.btnexit.Text = "Exit"
+        Me.btnexit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(407, 60)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(303, 87)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Food"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(436, 204)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(273, 121)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Drinks"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(420, 399)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(308, 114)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Sum Up"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lblSum
+        '
+        Me.lblSum.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.lblSum.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSum.Location = New System.Drawing.Point(736, 380)
+        Me.lblSum.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblSum.Name = "lblSum"
+        Me.lblSum.Size = New System.Drawing.Size(500, 95)
+        Me.lblSum.TabIndex = 2
+        '
+        'txtFood
+        '
+        Me.txtFood.Location = New System.Drawing.Point(743, 38)
+        Me.txtFood.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtFood.Multiline = True
+        Me.txtFood.Name = "txtFood"
+        Me.txtFood.Size = New System.Drawing.Size(492, 90)
+        Me.txtFood.TabIndex = 3
+        '
+        'txtDrinks
+        '
+        Me.txtDrinks.Location = New System.Drawing.Point(743, 181)
+        Me.txtDrinks.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtDrinks.Multiline = True
+        Me.txtDrinks.Name = "txtDrinks"
+        Me.txtDrinks.Size = New System.Drawing.Size(492, 100)
+        Me.txtDrinks.TabIndex = 3
+        '
+        'btnTotal
+        '
+        Me.btnTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTotal.Location = New System.Drawing.Point(21, 613)
+        Me.btnTotal.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnTotal.Name = "btnTotal"
+        Me.btnTotal.Size = New System.Drawing.Size(532, 201)
+        Me.btnTotal.TabIndex = 4
+        Me.btnTotal.Text = "Total"
+        Me.btnTotal.UseVisualStyleBackColor = True
+        '
+        'btnreset
+        '
+        Me.btnreset.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnreset.Location = New System.Drawing.Point(623, 613)
+        Me.btnreset.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnreset.Name = "btnreset"
+        Me.btnreset.Size = New System.Drawing.Size(583, 201)
+        Me.btnreset.TabIndex = 4
+        Me.btnreset.Text = "Reset"
+        Me.btnreset.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1856, 828)
+        Me.Controls.Add(Me.btnreset)
+        Me.Controls.Add(Me.btnTotal)
+        Me.Controls.Add(Me.txtDrinks)
+        Me.Controls.Add(Me.txtFood)
+        Me.Controls.Add(Me.lblSum)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnexit)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnexit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblSum As Label
+    Friend WithEvents txtFood As TextBox
+    Friend WithEvents txtDrinks As TextBox
+    Friend WithEvents btnTotal As Button
+    Friend WithEvents btnreset As Button
+End Class
---
+++ Year 1/Unit 14 - Event Driven Programming/Tutorial Programs/Exit/Exit/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub btnexit_Click(sender As Object, e As EventArgs) Handles btnexit.Click
+        Dim a As DialogResult
+        a = MessageBox.Show("Confirm you want to exit", "Exit system", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+        If a = DialogResult.Yes Then Application.Exit()
+    End Sub
+
+    Private Sub btnexit_MouseEnter(sender As Object, e As EventArgs) Handles btnexit.MouseEnter
+        btnexit.BackColor = Color.Red
+    End Sub
+
+    Private Sub btnexit_MouseLeave(sender As Object, e As EventArgs) Handles btnexit.MouseLeave
+        btnexit.BackColor = Color.FromKnownColor(KnownColor.Control)
+    End Sub
+
+    Private Sub lblSum_MouseEnter(sender As Object, e As EventArgs) Handles lblSum.MouseEnter
+        lblSum.BackColor = Color.White
+    End Sub
+
+    Private Sub lblSum_MouseLeave(sender As Object, e As EventArgs) Handles lblSum.MouseLeave
+        lblSum.BackColor = Color.FromKnownColor(KnownColor.Control)
+    End Sub
+
+    Private Sub btnreset_Click(sender As Object, e As EventArgs) Handles btnreset.Click
+        lblSum.Text = ""
+        txtFood.Text = ""
+        txtDrinks.Text = ""
+    End Sub
+
+    Private Sub btnTotal_Click(sender As Object, e As EventArgs) Handles btnTotal.Click
+        lblSum.Text = Val(txtFood.Text) + Val(txtDrinks.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
