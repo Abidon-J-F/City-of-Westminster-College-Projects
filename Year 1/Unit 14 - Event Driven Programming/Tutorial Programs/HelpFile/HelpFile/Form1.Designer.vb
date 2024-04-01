73b11dfa44fa0c6f463d07beee97377972689fb7
Abidon Jude Fernandes <106085589+Abidon-J-F@users.noreply.github.com>
2024-04-01 18:12:06 +0100

    Add files via upload

---
+++ Year 1/Unit 14 - Event Driven Programming/Tutorial Programs/HelpFile/HelpFile/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim TreeNode1 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Labour")
+        Dim TreeNode2 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Travel")
+        Dim TreeNode3 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Plastic")
+        Dim TreeNode4 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Copper")
+        Dim TreeNode5 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Chrome")
+        Dim TreeNode6 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Addition Button")
+        Dim TreeNode7 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Exit")
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TreeView1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.RichTextBox1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(574, 439)
+        Me.SplitContainer1.SplitterDistance = 191
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(0, 0)
+        Me.TreeView1.Name = "TreeView1"
+        TreeNode1.Name = "Node3"
+        TreeNode1.Text = "Labour"
+        TreeNode2.Name = "Node4"
+        TreeNode2.Text = "Travel"
+        TreeNode3.Name = "Node5"
+        TreeNode3.Text = "Plastic"
+        TreeNode4.Name = "Node6"
+        TreeNode4.Text = "Copper"
+        TreeNode5.Name = "Node7"
+        TreeNode5.Text = "Chrome"
+        TreeNode6.Name = "Node8"
+        TreeNode6.Text = "Addition Button"
+        TreeNode7.Name = "Node9"
+        TreeNode7.Text = "Exit"
+        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode1, TreeNode2, TreeNode3, TreeNode4, TreeNode5, TreeNode6, TreeNode7})
+        Me.TreeView1.Size = New System.Drawing.Size(188, 439)
+        Me.TreeView1.TabIndex = 0
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(-1, 0)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(380, 439)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(574, 439)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ Year 1/Unit 14 - Event Driven Programming/Tutorial Programs/HelpFile/HelpFile/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
+        If e.Node.Name = "Node3" Then
+            RichTextBox1.Text = "On the Insert tab, the galleries include items that are designed"
+
+        End If
+
+        If e.Node.Name = "Node4" Then
+            RichTextBox1.Text = "Both the Themes gallery and the Quick Styles gallery provide reset"
+        End If
+
+        If e.Node.Name = "Node5" Then
+            RichTextBox1.Text = "To change th elooks avaible in the Quick Style gallery, use the Change Current Quick Style Set"
+        End If
+
+        If e.Node.Name = "Node6" Then
+            RichTextBox1.Text = "To change th elooks avaible in the Quick Style gallery, use the Change Current Quick Style Set"
+        End If
+
+        If e.Node.Name = "Node7" Then
+            RichTextBox1.Text = "To change th elooks avaible in the Quick Style gallery, use the Change Current Quick Style Set"
+        End If
+
+        If e.Node.Name = "Node8" Then
+            RichTextBox1.Text = "To change th elooks avaible in the Quick Style gallery, use the Change Current Quick Style Set"
+        End If
+
+        If e.Node.Name = "Node9" Then
+            Me.Hide()
+        End If
+    End Sub
+End Class
